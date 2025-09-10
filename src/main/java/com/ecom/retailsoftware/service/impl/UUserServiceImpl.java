fa1f1ea8d9a0595f56f303c5cb51ad76d11c5302
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-09-09 20:36:07 -0500

    Implemented UserServiceImpl class

---
+++ src/main/java/com/ecom/retailsoftware/service/impl/UUserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.ecom.retailsoftware.service.impl;
+
+import com.ecom.retailsoftware.entity.UserEntity;
+import com.ecom.retailsoftware.io.UserRequest;
+import com.ecom.retailsoftware.io.UserResponse;
+import com.ecom.retailsoftware.repository.UserRepository;
+import com.ecom.retailsoftware.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class UUserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserResponse createUser(UserRequest request) {
+        UserEntity newUser = convertToEntity(request);
+        newUser = userRepository.save(newUser);
+        return convertToResponse(newUser);
+    }
+
+    private UserResponse convertToResponse(UserEntity newUser) {
+        return UserResponse.builder()
+                .name(newUser.getName())
+                .email(newUser.getEmail())
+                .userId(newUser.getUserId())
+                .createdAt(newUser.getCreatedAt())
+                .updatedAt(newUser.getUpdatedAt())
+                .role(newUser.getRole())
+                .build();
+    }
+
+    private UserEntity convertToEntity(UserRequest request) {
+        return UserEntity.builder()
+                .userId(UUID.randomUUID().toString())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole().toUpperCase())
+                .name(request.getName())
+                .build();
+    }
+
+    @Override
+    public String getUserRole(String email) {
+        UserEntity existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found for email: " + email));
+        return existingUser.getRole();
+    }
+
+    @Override
+    public List<UserResponse> readUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> convertToResponse(user))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        UserEntity existingUser = userRepository.findByUserId(id)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        userRepository.delete(existingUser);
+    }
+}
