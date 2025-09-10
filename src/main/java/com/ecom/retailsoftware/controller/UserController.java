38f49408da28d41918e6aa1e81da67493a7a29a5
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-09-09 20:55:29 -0500

    Created endpoints for user profiles in UserController.java

---
+++ src/main/java/com/ecom/retailsoftware/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.ecom.retailsoftware.controller;
+
+
+import com.ecom.retailsoftware.io.UserRequest;
+import com.ecom.retailsoftware.io.UserResponse;
+import com.ecom.retailsoftware.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserResponse registerUser(@RequestBody UserRequest userRequest) {
+        try {
+            return userService.createUser(userRequest);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create user");
+        }
+    }
+
+    @GetMapping("/users")
+    public List<UserResponse> readUsers() {
+        return userService.readUsers();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable String id) {
+        try {
+            userService.deleteUser(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to delete user");
+        }
+    }
+
+}
